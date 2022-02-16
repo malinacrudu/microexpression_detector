d355d6701b677295a4c5854b9b2372c14038de1b
mariacob <79450857+mariacob@users.noreply.github.com>
2022-02-16 10:50:55 +0200

    update haar_cascade.py

--- microExpressions/haar_cascade.py
+++ microExpressions/haar_cascade.py
@@ -1,38 +1,44 @@
 import cv2
 import glob
+import math
+
 
 def detect_eyes(img):
     # Read in the cascade classifiers for eyes
     eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
     eye_img = img.copy()
-    eye_rect = eye_cascade.detectMultiScale(eye_img, scaleFactor=1.2, minNeighbors=6)
+    eye_rect = eye_cascade.detectMultiScale(eye_img, 1.05, 3)
+
     if len(eye_rect) < 2:
         return
-    min_1 = eye_rect[0][1]
-    min_2 = eye_rect[1][1]
-    eye_right = eye_rect[0]
-    eye_left = eye_rect[1]
+
+    min_1 = math.inf
+    min_2 = math.inf
+    eye_1 = []
+    eye_2 = []
+    # print(eye_rect)
     for obj in eye_rect:
         if obj[1] < min_1:
             min_2 = min_1
             min_1 = obj[1]
-            if obj[0] < eye_right[0]:
-                eye_right = obj
-            else:
-                eye_left = obj
-        if obj[1] < min_2:
+            eye_2 = eye_1
+            eye_1 = obj
+        if obj[1] < min_2 and obj[0] != eye_1[0]:
             min_2 = obj[1]
-            if obj[0] < eye_right[0]:
-                eye_right = obj
-            else:
-                eye_left = obj
-
-    max_width = max(eye_right[2], eye_left[2])
-    start_y = min(eye_right[1], eye_left[1])
+            eye_2 = obj
+    # print("-----------------------")
+    # print(eye_1, eye_2)
+    max_width = max(eye_1[2], eye_2[2])
+    start_y = min(eye_1[1], eye_2[1])
     end_y = start_y + max_width
-    start_x = eye_right[0]
-    end_x = eye_left[0] + max_width
-    cropped = img[36:36 + 65, 11:119 + 65]
+    start_x = min(eye_1[0], eye_2[0])
+    end_x = max(eye_1[0], eye_2[0]) + max_width
+    # print(max_width)
+    # print(start_y)
+    # print(end_y)
+    # print(start_x)
+    # print(end_x)
+    cropped = img[start_y:end_y, start_x:end_x]
     # print(cropped)
     # for (x, y, w, h) in eye_rect:
     #     cv2.rectangle(eye_img, (x, y), (x + w, y + h), (255, 255, 255), 10)
@@ -40,9 +46,16 @@ def detect_eyes(img):
 
 
 def haar_cascade():
-    images = glob.glob('HaarCascade/Cropped-updated/Cropped/*/*/*.jpg', recursive=True)
+    images = glob.glob('Haar3/Cropped-updated/Cropped/*/*/*.jpg', recursive=True)
+    nr = 0
     for imgPath in images:
         image = cv2.imread(imgPath)
         cropped = detect_eyes(image)
         if cropped is not None:
             cv2.imwrite(imgPath, cropped)
+        else:
+            cv2.imwrite('greseli3/' + str(nr) + '.jpg', image)
+            nr += 1
+    # image = cv2.imread('test.jpg')
+    # cropped = detect_eyes(image)
+    # cv2.imwrite('rez.jpg',cropped)
